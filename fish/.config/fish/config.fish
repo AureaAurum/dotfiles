5480603e4a231034d3d8947496016d2d24607552
Aurea Aurum <your_email@no-reply.com>
2026-01-07 00:10:06 +0900

    add mise-code integrate

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,7 +21,11 @@ if status is-interactive
 
     # Initialize Mise (これを忘れると言語が動きません！)
     if type -q mise
-        mise activate fish | source
+        if status is-interactive
+            mise activate fish | source
+        else
+            mise activate fish --shims | source
+        end
     end
 
     # Initialize Starship
